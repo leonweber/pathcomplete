fffecfa0075fa337e250a0f6a7d821cff06705bc
leonweber@posteo.de <tL7rHcdCLzV4>
2019-09-11 12:32:59 +0200

    update e

--- conversion/json_to_link_prediction.py
+++ conversion/json_to_link_prediction.py
@@ -8,24 +8,25 @@ if __name__ == '__main__':
     parser.add_argument('--name', required=True)
     args = parser.parse_args()
 
-    os.makedirs(f'{datadir}/link_prediction/{args.name}', exist_ok=True)
+    out_dir = f'{datadir}/../link_prediction/data/{args.name}'
+    os.makedirs(out_dir, exist_ok=True)
 
     with open(f'{datadir}/PathwayCommons11.{args.name}.hgnc.txt.train.json') as f:
         train = json.load(f)
         train_triples = [t.replace(',', '\t') for t in train]
-    with open(f"{datadir}/link_prediction/{args.name}/train.txt", 'w') as f:
+    with open(f"{out_dir}/train.txt", 'w') as f:
         f.write("\n".join(train_triples))
 
     with open(f'{datadir}/PathwayCommons11.{args.name}.hgnc.txt.dev.json') as f:
         dev = json.load(f)
         dev_triples = [t.replace(',', '\t') for t in dev]
-    with open(f"{datadir}/link_prediction/{args.name}/valid.txt", 'w') as f:
+    with open(f"{out_dir}/valid.txt", 'w') as f:
         f.write("\n".join(dev_triples))
 
     with open(f'{datadir}/PathwayCommons11.{args.name}.hgnc.txt.test.json') as f:
         test = json.load(f)
         test_triples = [t.replace(',', '\t') for t in test]
-    with open(f"{datadir}/link_prediction/{args.name}/test.txt", 'w') as f:
+    with open(f"{out_dir}/test.txt", 'w') as f:
         f.write("\n".join(test_triples))
 
     entities = set()
--- evaluation/evaluate_provenance.py
+++ evaluation/evaluate_provenance.py
@@ -3,6 +3,7 @@ import itertools
 import json
 from collections import defaultdict
 from copy import deepcopy
+from pprint import pprint
 
 import numpy as np
 import pandas as pd
@@ -24,61 +25,23 @@ def augment_preds(preds):
     return augmented_preds
 
 
-def filter_triples(triples, entities):
-    filtered_triples = set()
-    for triple in triples:
-        if triple.split(",")[1] == "NA":
-            continue
-        e1 = triple.split(",")[0]
-        e2 = triple.split(",")[2]
-
-        if e1 in entities and e2 in entities:
-            filtered_triples.add(triple)
-
-    return filtered_triples
 
 
-def evaluate_relations(anns, preds, entities=None, baseline=None, has_scores=True):
+def evaluate_relations(anns, preds, baseline=None):
 
     true_relations = set(anns)
 
-    if baseline:
-        baseline_prec = evaluate_relations(anns, baseline, entities, has_scores=False)['prec']
-        pred_relations = list(reversed(sorted(preds, key=lambda x: float(preds[x]["score"]))))
-        precs = []
-        for i, _ in enumerate(pred_relations, start=1):
-            current_preds = set(pred_relations[:i])
-            if entities:
-                current_preds = filter_triples(current_preds, entities)
-            tps = current_preds & true_relations
-            precs.append(len(tps) / i)
-        precs = np.array(precs)
-        try:
-            i = np.where(precs >= baseline_prec)[0][-1]
-            pred_relations = set(pred_relations[:i])
-        except IndexError:
-            pred_relations = set()
-    else:
-        pred_relations = set(p for p, meta in preds.items() if "score" not in meta or meta["score"] >= 0.6)
+    proba_scores = [preds[rel]["score"] for rel in preds]
+    y_true = [int(rel in true_relations) for rel in preds]
 
-    if has_scores:
-        proba_scores = [preds[rel]["score"] for rel in preds]
-        y_true = [int(rel in true_relations) for rel in preds]
+    proba_scores = proba_scores + [min(proba_scores)-1] * len(true_relations - set(preds))
+    y_true = y_true + [1] * len(true_relations - set(preds))
 
-        if proba_scores:
-            proba_scores = proba_scores + [min(proba_scores)-1] * len(true_relations - set(preds))
-            y_true = y_true + [1] * len(true_relations - set(preds))
+    prec_vals, rec_vals, _ = precision_recall_curve(y_true, proba_scores)
+    ap = np.sum(np.diff(np.insert(rec_vals[::-1], 0, 0)) * prec_vals[::-1])
 
-            prec_vals, rec_vals, _ = precision_recall_curve(y_true, proba_scores)
-
-            prec_vals = prec_vals.tolist()
-            rec_vals = rec_vals.tolist()
-        else:
-            prec_vals = []
-            rec_vals = []
-    else:
-        prec_vals = None
-        rec_vals = None
+    prec_vals = prec_vals.tolist()
+    rec_vals = rec_vals.tolist()
 
 
     if baseline and preds:
@@ -86,62 +49,20 @@ def evaluate_relations(anns, preds, entities=None, baseline=None, has_scores=Tru
         proba_scores = [preds[rel]["score"] for rel in preds if rel not in baseline]
 
         new_prec_vals, new_rec_vals, _ = precision_recall_curve(y_true, proba_scores)
+        new_ap = np.sum(np.diff(np.insert(new_rec_vals[::-1], 0, 0)) * new_prec_vals[::-1])
+
         new_prec_vals = new_prec_vals.tolist()
         new_rec_vals = new_rec_vals.tolist()
     else:
         new_prec_vals = None
         new_rec_vals = None
+        new_ap = None
 
-    if entities:
-        pred_relations = filter_triples(pred_relations, entities)
-        true_relations = filter_triples(true_relations, entities)
-
-    tps = true_relations & pred_relations
-    fps = pred_relations - true_relations
-    fns = true_relations - pred_relations
-
-    if baseline:
-        tps_new = len(tps - set(baseline))
-    else:
-        tps_new = None
-
-    if len(tps) == 0:
-        return {
-        'prec': 0,
-        'rec': 0,
-        'f1': 0,
-        'support': len(true_relations),
-        'new': 0,
-        'prec_vals': prec_vals,
-        'rec_vals': rec_vals,
-        'new_prec_vals': new_prec_vals,
-        'new_rec_vals': new_rec_vals,
-    }
-
-    prec = len(tps) / (len(tps) + len(fps))
-    rec = len(tps) / (len(tps) + len(fns))
-
-    if prec == 0 or rec == 0:
-        return {
-            'prec': 0,
-            'rec': 0,
-            'f1': 0,
-            'support': len(true_relations),
-            'new': 0,
-            'prec_vals': prec_vals,
-            'rec_vals': rec_vals,
-            'new_prec_vals': new_prec_vals,
-            'new_rec_vals': new_rec_vals,
-        }
-
-    f1 = 2 * prec * rec / (prec + rec)
 
     result = {
-        'prec': prec,
-        'rec': rec,
-        'f1': f1,
         'support': len(true_relations),
-        'new': tps_new,
+        'ap': ap,
+        'new_ap': new_ap,
         'prec_vals': prec_vals,
         'rec_vals': rec_vals,
         'new_prec_vals': new_prec_vals,
@@ -232,7 +153,10 @@ if __name__ == '__main__':
 
     with open(args.preds) as f:
         preds = json.load(f)
-    preds = augment_preds(preds)
+        if args.filter:
+            with open(args.filter) as f:
+                filter = json.load(f)
+            preds = {k: v for k, v in preds.items() if k not in filter}
 
     entities = set()
     if args.pathway:
@@ -241,9 +165,11 @@ if __name__ == '__main__':
             for triple in pw_triples:
                 entities.add(triple.split(',')[0])
                 entities.add(triple.split(',')[2])
-    else:
-        entities = None
 
+        anns = {k: v for k, v in anns.items() if k.split(',')[0] in entities and k.split(',')[2] in entities}
+        preds = {k: v for k, v in preds.items() if k.split(',')[0] in entities and k.split(',')[2] in entities}
+
+    preds = augment_preds(preds)
 
     df = defaultdict(list)
 
@@ -267,10 +193,10 @@ if __name__ == '__main__':
             rel_anns = deepcopy(anns)
             rel_preds = deepcopy(preds)
 
-        res = evaluate_relations(rel_anns, rel_preds, entities, baseline, has_scores=not args.no_scores)
+        res = evaluate_relations(rel_anns, rel_preds, baseline)
 
-        new_str = f", New: {res['new']}" if res['new'] else ""
-        print(f"{relation}: {res['prec']}/{res['rec']}/{res['f1']}{new_str}, Support: {res['support']}")
+        print(relation)
+        pprint({k: v for k,v in res.items() if 'vals' not in k})
         df['rel'].append(relation)
 
         for r, value in res.items():
